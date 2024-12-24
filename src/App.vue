4d56c9aa7442b462447040685e77b75f0d27dcc6
Logan <pix.59700@gmail.com>
2024-12-24 21:03:07 +0100

    Setup navbar and first transition

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script>
+     import './assets/App.css'
+
      import Home from './components/Home.vue'
      import About from './components/About.vue'
      import NotFound from './components/NotFound.vue'
@@ -28,9 +30,14 @@
 </script>
 
 <template>
-     <a href="#/">Home</a> |
-     <a href="#/about">About</a> |
-     <a href="#/non-existent-path">Broken Link</a>
+     <nav class="navbar">
+          <a href="#/"><img src="../public/logo.svg" alt="" class="logo"> portfolio - delachapelle logan</a>
+          <ul class="nav-links">
+               <li><a href="#/about">about</a></li>
+               <li><a href="#/projects">projects</a></li>
+               <li><a href="#/contact">contact</a></li>
+          </ul>
+     </nav>
 
      <Transition name="fade" mode="out-in">
           <component :is="currentView"/>
@@ -40,13 +47,17 @@
 
 <style scoped>
 
-.fade-enter-active,
-.fade-leave-active {
-     transition: opacity 0.5s ease;
+.fade-enter-active {
+     transition: all 0.3s ease-out;
 }
 
-.fade-enter-form,
+.fade-leave-active {
+     transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.fade-enter-from,
 .fade-leave-to {
+     transform: translateY(20px);
      opacity: 0;
 }
 
